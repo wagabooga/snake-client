ec4903a695f39fd624aafe2fc54ae3970c6a1e0f
wagabooga <mattanthu@hotmail.com>
2021-09-25 01:02:47 +0000

    added setupInput and handleUserInput to play.js

--- play.js
+++ play.js
@@ -1,2 +1,16 @@
 const { connect } = require("./client.js");
 const net = require("net");
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+stdin.on("data", handleUserInput);
+
+const handleUserInput = function (data) {
+  setupInput()
+}
